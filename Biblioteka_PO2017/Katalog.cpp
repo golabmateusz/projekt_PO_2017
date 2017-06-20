5e54663d7c200b75d11417da979bc804c89309b9
k-juchniewicz <Temil413@wp.pl>
2017-06-21 00:56:08 +0200

    nastepny

--- Biblioteka_PO2017/Katalog.cpp
+++ Biblioteka_PO2017/Katalog.cpp
@@ -2,73 +2,6 @@
 #include "Katalog.h"
 #include "logowanie.h"
 using namespace std;
-/*
-void Katalog::wyswietl()
-{
-   cout<<"Wyswietla wszystkie pozycje :";
-   cout<<Pozycja(nazwa*ptr)
-
-   cout << endl << "Prosze bardzo " << numer << " ksiazki" << endl;
-  for (int n = 0; n < numer; n++)
-    cout << setw(12) << zbior[n].nazwa << setw(6) << " by " << zbior[n].autor() << endl;
-
-
-}
-void Katalog::dodaj(Ksiazka x)
-{
-    zbior[numer++] = x;
-   cout<<"DODAJ POZYCJE!";
-
-}
-void Katalog::usun()
-{
-    for (int n = 0; n < numer; n++)
-    if (zbior[n] == x) {
-      for (int nn = n + 1; nn < numer; nn++)
-        zbior[nn - 1] = zbior[nn];
-      cout << x.nazwa << " by " << x.autor << " deleted" << endl;
-      --numer;
-      return;
-    }
-  cout << x.nazwa() << " not found" << endl;
-   cout<<"USUN POZYCJE";
-}
-
-void zbior::run() {
-  Katalog kat;
-  string nazwa, autor;
-
-  cout << "Podaj autora ";
-  cin >> autor;
-  cout << "Podaj nazwe";
-  cin >> nazwa;
-  cout << "Podaj rok";
-  cin >> rokWydania;
-  cout << "Podaj status";
-  cin >> status;
-  cout << "Podaj liczbaStron";
-  cin >> liczbaStron;
-
-  while (!cin.eof()) {
-    kat.dodaj(Ksiazka(autor, nazwa, rokWydania, status, liczbaStron));
-
-  cout << "Podaj autora ";
-  cin >> autor;
-  cout << "Podaj nazwe";
-  cin >> nazwa;
-  cout << "Podaj rok";
-  cin >> rokWydania;
-  cout << "Podaj status";
-  cin >> status;
-  cout << "Podaj liczbaStron";
-  cin >> liczbaStron;
-  }
-  kat.wyswietl();  // print the library holdings
-  kat.dodaj(Book("Tomek", "Einstein"));  // delete if present
-  kat.usun(Book("Bend", "Hartley"));      // delete if present
-  kat.wyswietl();  // print holdings again to verify deletions
-}
-*/
 
 Katalog::Katalog()
 {
@@ -77,65 +10,114 @@ Katalog::Katalog()
 
 void Katalog::dostep_do_katalogu()
 {
+    int wybory = 0;
     if(&Logowanie::ktojestkto)//uzytkownik
     {
         cout << "katalog user";
+
+        cout<< "\nWitaj w katalogu uzytkowniku!\nWybierz:\n1. Lista ksiazek\n2. Wypozycz ksiazke\n3. Oddaj ksiazke\n4. Wyloguj\n";
+        cin>>wybory;
+        switch (wybory)
+        {
+        case 1:
+            this->wyswietl();
+             break;
+        case 2:
+            this->wypozycz();
+             break;
+        case 3:
+            this->oddaj();
+             break;
+        case 4:
+            this->wyloguj();
+            break;
+        default:
+            break;
+        }
+
     }
     else
     {
         cout << "katalog ten drugi";
+         cout<< "\nWitaj w katalogu Bibliotekarzu!\nWybierz:\n1. Lista ksiazek\n2. Zmien status/wypozycz\n3. Usun ksiazke\n4. Usun konto\n5. Wyloguj\n";
+        cin>>wybory;
+        switch (wybory)
+        {
+        case 1:
+            this->wyswietl();
+             break;
+        case 2:
+           // this->zmienStatus();
+             break;
+        case 3:
+            this->usun();
+             break;
+        case 4:
+            this->usun_konto();
+            break;
+        case 5:
+            this->wyloguj();
+            break;
+        default:
+            break;
+        }
     }
 }
 
 Katalog::~Katalog()
-{/*
+{
     for(size_t i=0; i<this->zbior.size(); ++i)
     {
-        delete this->zbior.at(i);
+        if (this->zbior.at(i) != NULL)
+            delete this->zbior.at(i);
     }
     this->zbior.clear();
-    //usuwanie pozycji z wektora
-    */
-}
-/*
-void Katalog::manager()
-{
-    int wybor = 0;
-    cout << "\nManager ksiazek!\nWybierz:\n1. Dodaj pozycje\n2. Usun pozycje\n";
-    cin>>wybor;
-    switch (wybor)
-    {
-    case 2:
-        this->dodaj();
-        break;
-    default:
-        this->usun();
-        break;
-    }
-}
-*/
-void Katalog::usun(std::string autor, std::string nazwa)
-{
-    /*
-    string haslo = "";
-    string login = "";
-    cout << "Zaloguj sie\nPodaj login: ";
-    cin >> login;
-    cout << "\nPodaj haslo: ";
-    cin >> haslo;
-    cout << "\n";
-    this->szukaj(haslo, login);
-    */
+    //usuwanie osob z wektora
+
 }
 
-void Katalog::dodaj(std::string autor, std::string nazwa, int rokWydania, std::string status)
+void Katalog::usun(std::vector<Pozycja*> &zbior)
 {
-    /*
+     unsigned int wybor = 0;
+    std::cout << "Lista pozycji systemie:\n";
+
+    for(size_t i=0; i< zbior.size(); ++i)
+    {
+        try
+        {
+            if (zbior.at(i) != NULL)
+                std::cout << i <<". "<< zbior.at(i)->login <<"\n";
+        }
+        catch(...)
+        {
+            std::cout << i << "cos sie zepsulo...\n";
+        }
+    }
+    std::cout << "\nWybierz nr pozycji, ktora chcesz usunac, lub wartosc spoza zakresu aby zakonczyc: ";
+    std::cin >> wybor;
+    std::cout << "\n";
+    if(wybor < (zbior.size()))
+    {
+        std::cout << "usunieto konto " << zbior.at(wybor)-> login << "\n";
+        if (zbior.at(wybor) != NULL)
+            delete zbior.at(wybor);
+        zbior.at(wybor) = NULL;
+    }
+
+    else
+    {
+        std::cout << "nie usunieto zadnej pozycji\n";
+    }
+
+}
+
+void Katalog::dodaj()
+{
     RodzajPozycji typ;
     int wybor = 0;
-    cout<< "\nJaka pozycje chcesz dodac:\n1. Ksiazka\n";
+     cout<< "\nDodawanie pozycji:k\n";
+    cout<< "\nJesli chcesz dodac nowa Ksiazke do kolekcji nacisnij 1 \n";
     cin >> wybor;
-    cout<< "\n";
     switch (wybor)
     {
     case 1:
@@ -148,16 +130,46 @@ void Katalog::dodaj(std::string autor, std::string nazwa, int rokWydania, std::s
     AbstrakcyjnaFabrykaPozycji* fabryka = AbstrakcyjnaFabrykaPozycji::wybierzFabryke(typ);
     zbior.push_back(fabryka->stworzPozycje());
     delete fabryka;
-    this->manager();
-    */
+    this->dostep_do_katalogu();
+
 }
 void Katalog::oddaj()
 {
 
 }
 
-void Katalog::usun_konto()
+void Katalog::usun_konto(std::vector<Osoba*> &lista)
 {
+     unsigned int wybor = 0;
+    std::cout << "Lista loginow kont w systemie:\n";
+
+    for(size_t i=0; i< lista.size(); ++i)
+    {
+        try
+        {
+            if (lista.at(i) != NULL)
+                std::cout << i <<". "<< lista.at(i)->login <<"\n";
+        }
+        catch(...)
+        {
+            std::cout << i << "cos sie zepsulo...\n";
+        }
+    }
+    std::cout << "\nWybierz nr konta ktore chcesz usunac, lub wartosc spoza zakresu aby zakonczyc: ";
+    std::cin >> wybor;
+    std::cout << "\n";
+    if(wybor < (lista.size()))
+    {
+        std::cout << "usunieto konto " << lista.at(wybor)-> login << "\n";
+        if (lista.at(wybor) != NULL)
+            delete lista.at(wybor);
+        lista.at(wybor) = NULL;
+    }
+
+    else
+    {
+        std::cout << "nie usunieto zadnego konta\n";
+    }
 
 }
 
